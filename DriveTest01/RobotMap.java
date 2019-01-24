4d83f546e0f1a0eb08eca7849e3135327079b9c2
SBHSRobotics6884 <35392817+SBHSRobotics6884@users.noreply.github.com>
2019-01-23 17:55:39 -0800

    Add files via upload

---
+++ DriveTest01/RobotMap.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+  public static final int L2_DRIVETRAIN = 1; //C
+	public static final int L1_DRIVETRAIN = 0; //C
+	public static final int R1_DRIVETRAIN = 2; //C
+  public static final int R2_DRIVETRAIN = 3; //C 
+  
+  public static final int PCM_ID = 0;
+}
